3ab07af040f0f19b6b04cc28e8e11a16e559ebb6
protoDUNE-SP <pdsp@apollo.phy.bnl.gov>
2019-05-09 16:34:40 -0400

    use sed to change files

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://cdcvs.fnal.gov/redmine/projects/larwirecell/repository
-larsoft_version=v08_08_00
-larwirecell_version=v08_02_03
+larsoft_version=v08_18_00
+larwirecell_version=v08_05_01
 sl7img=
 
 clear
@@ -39,16 +39,20 @@ fi
 
 wcdo.sh make-project myproj sl7
 
-echo
-echo "Please fill the larsoft version in wcdo-local-myproj.rc as follows (default editor: vim)."
-echo
-echo wcdo_mrb_project_name='"'larsoft'"'
-echo wcdo_mrb_project_version='"'$larsoft_version'"'
-echo wcdo_mrb_project_quals='"'e17:prof'"'
-echo
-read -p "Press [Enter] key to continue..."
+# echo
+# echo "Please fill the larsoft version in wcdo-local-myproj.rc as follows (default editor: vim)."
+# echo
+# echo wcdo_mrb_project_name='"'larsoft'"'
+# echo wcdo_mrb_project_version='"'$larsoft_version'"'
+# echo wcdo_mrb_project_quals='"'e17:prof'"'
+# echo
+# read -p "Press [Enter] key to continue..."
+# 
+# vim wcdo-local-myproj.rc
 
-vim wcdo-local-myproj.rc
+sed -i '/^wcdo_mrb_project_name/ s/.*/wcdo_mrb_project_name="'larsoft'"/g' wcdo-local-myproj.rc
+sed -i '/^wcdo_mrb_project_version/ s/.*/wcdo_mrb_project_version="'$larsoft_version'"/g' wcdo-local-myproj.rc
+sed -i '/^wcdo_mrb_project_quals/ s/.*/wcdo_mrb_project_quals="'e17:prof'"/g' wcdo-local-myproj.rc
 
 echo
 echo "Now the singularity container will start. Wirecell toolkit and LarWirecell package will be installed."
@@ -62,77 +66,52 @@ source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 path-prepend \$wcdo_ups_products PRODUCTS
 wcdo-mrb-init
 wcdo-mrb-add-source larwirecell $larwirecell_version $larwirecell_version
-
 wcdo-ups-declare wirecell wctdev
 setup wirecell wctdev -q e17:prof
-
 wcdo-ups-wct-configure-source
 ./wcb -p --notests install
 setup wirecell wctdev -q e17:prof
 wcdo-wirecell-path default
-
 echo
-echo "Please change the wirecell version as follows (default editor: vim)."
-echo "wirecell v0_10_5 ==> wirecell wctdev"
-echo
-read -p "Press [Enter] key to continue..."
 
-vim /wcdo/src/mrb/srcs/larwirecell/ups/product_deps
+# echo "Please change the wirecell version as follows (default editor: vim)."
+# echo "wirecell v0_xx_xx ==> wirecell wctdev"
+# echo
+# read -p "Press [Enter] key to continue..."
+# vim /wcdo/src/mrb/srcs/larwirecell/ups/product_deps
+
+sed -i '/^wirecell/ s/.*/wirecell        wctdev/g' /wcdo/src/mrb/srcs/larwirecell/ups/product_deps 
 
 mrbsetenv
 mrb i
 mrbslp
-
 echo
 echo "--- test 1: ups active | grep wirecell"
 ups active | grep wirecell
 echo
 echo "--- test 2: ups list -aK+ wirecell | head"
 ups list -aK+ wirecell | head
-
 echo "Successful installation!"
-echo "Here is an example of wcdo-local-myproj.rc"
-echo "After you update your wcdo-local-myproj.rc, try ./wcdo-myproj.sh"
-echo
+
+#echo "Here is an example of wcdo-local-myproj.rc"
+#echo "After you update your wcdo-local-myproj.rc, try ./wcdo-myproj.sh"
+#echo
+
 read -p "Press [Enter] key to continue..."
 
-
-cat << EOF
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#!/bin/bash
-
-# This is a local wcdo rc file for project myproj.
-# It was initally generated but is recomended for customizing by you, dear user.
-# It is included at the end of the main RC files.
-    
-# These are optional but required if wcdo-mrb-* commands are to be used.
-wcdo_mrb_project_name="larsoft"
-wcdo_mrb_project_version="$larsoft_version"
-wcdo_mrb_project_quals="e17:prof"
-
-# Additional variables may be usefully set since this file was
-# first generated.  
-
-# It is perhaps useful to end this with some command to be called 
-# on each entry to the contaner.
-# source /cvmfs/larsoft.opensciencegrid.org/products/setup
+cat << EOF >> /wcdo/wcdo-local-myproj.rc
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup dunetpc $larsoft_version -q e17:prof
 path-prepend \\\$wcdo_ups_products PRODUCTS
 wcdo-mrb-init
 wcdo-ups-init
-
 setup wirecell wctdev -q e17:prof
 export WIRECELL_PATH=/wcdo/src/wct/cfg:/wcdo/share/wirecell/data
 echo WIRECELL_PATH=\\\$WIRECELL_PATH
-
 mrbsetenv
 mrbslp
 export FHICL_FILE_PATH=\\\$WIRECELL_PATH:\\\$FHICL_FILE_PATH
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 EOF
-
 EOL
 
 chmod +x postinstall.sh
