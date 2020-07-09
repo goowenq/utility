a2be9951306ba74cfb54411f6d7f1a7ced233eba
Wenqiang Gu <goowenq@gmail.com>
2020-07-09 17:35:13 -0400

    add find in python

---
+++ py/find.py
@@ -0,0 +1,18 @@
+import os
+def find_all(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+    return result
+
+import fnmatch
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+find('*.txt', '/path/to/dir')
